4bc360ca20aa5031c89151af4859d6d16c24dad7
Nimeh Jadallah <nimeh_abueid@hotmail.com>
2019-03-07 14:14:36 -0500

    working on Responsiveness

---
+++ assets/javascript/games.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+    var target="";
+    var redValue="";
+    var blueValue="";
+    var purpleValue="";
+    var greenValue="";
+    var redBtn;
+    var blueBtn;
+    var purpleBtn;
+    var greenBtn;
+    var score;
+    var wins=0;
+    var loss=0;
+    
+    function initialize() {
+        target=Math.floor(Math.random()*101)+19;
+        // reset counter
+        score=0;
+        var crystal=[$(".red"), $(".blue"), $(".purple"), $(".green")];
+        var button=[redBtn, blueBtn, purpleBtn, greenBtn];
+        // to assign value to each crystal
+        for (var i=0; i<crystal.length; i++) {
+            button[i]=crystal[i].attr("data-crystalValue", Math.floor(Math.random()*12)+2);
+        };
+        // to print out target number and score on screen
+        $("#target").html(target);
+        $("#wins").html(wins);
+        $("#losses").html(loss);
+        $("#score").html(score);
+    }
+    initialize();
+    
+    // console.log(redValue +"<br>"+blueValue+"<br>"+purpleValue+"<br>"+greenValue);
+    
+    $(".crystal").on("click", function(){
+        score += parseInt($(this).attr("data-crystalValue"));
+        $("#score").html(score);
+        if(score == target) {
+           alert("You Win!");
+            wins++;
+            initialize();
+        }
+        else if (score > target) {
+           alert("You Lose. Try Again!");
+            loss++;
+            initialize();
+        }
+    });
+    });
